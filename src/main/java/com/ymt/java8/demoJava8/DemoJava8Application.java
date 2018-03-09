f24a3c329838cb608a91b60d80e04179937eb058
YURI MICHAEL <mmontanezt@gmail.com>
2018-03-08 23:17:37 -0500

    first

---
+++ src/main/java/com/ymt/java8/demoJava8/DemoJava8Application.java
@@ -0,0 +1,12 @@
+package com.ymt.java8.demoJava8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJava8Application.class, args);
+	}
+}
